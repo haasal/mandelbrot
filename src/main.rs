a376d81927569593bf840a8c3a847e64b20fb99c
Alexander Haas <mail@haas-alexander.com>
2023-07-03 10:05:17 +0200

    refactor: extracted complex struct into own file

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,17 @@
-use std::{
-    fmt::Display,
-    io::{self, Stdin, Write},
-    ops::{Add, Mul},
-};
+mod complex;
 
+use std::io::{self, Write};
+
+use complex::C;
 use termion::{
     event::{Event, Key, MouseEvent},
     input::{MouseTerminal, TermRead},
     raw::IntoRawMode,
 };
 
-#[derive(Debug, Clone, Copy)]
-struct C {
-    pub im: f64,
-    pub re: f64,
-}
-
-impl Display for C {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} + i * {}", self.re, self.im)
-    }
-}
-
-impl C {
-    fn norm(self) -> f64 {
-        self.im * self.im + self.re * self.re
-    }
-}
-
-impl From<(f64, f64)> for C {
-    fn from(value: (f64, f64)) -> Self {
-        C {
-            re: value.0,
-            im: value.1,
-        }
-    }
-}
-
 // (a + ib) * (u + iw)
 // (a * u + i * a * w + i * b * u - b * w)
 // (a * u - b * w) + i (a * w + b * u)
-
-impl Mul for C {
-    type Output = Self;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        C {
-            re: self.re * rhs.re - self.im * rhs.im,
-            im: self.re * rhs.im + self.im * rhs.re,
-        }
-    }
-}
-
-impl Add for C {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        C {
-            re: self.re + rhs.re,
-            im: self.im + rhs.im,
-        }
-    }
-}
 
 fn convert_term_coords(
     term_x: u16,
